d2936a2ea5633254365385cdf004a9900b678689
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2026-08-04 22:31:56 +0300

    add sell_fraction dict

--- app.py
+++ app.py
@@ -67,6 +67,7 @@ class Configuration:
     cooldown_days: int
     cooldown_wait: int
     manual_sell_fraction: float
+    sell_fraction: dict
     enable_sell: bool
     enable_model: bool
     enable_ma200: bool
@@ -164,28 +165,33 @@ class BacktestDCA:
         
         ma200_sell = sell_ma200(
             prices=self.config.prices.loc[:date],
-            threshold=self.config.threshold_ma200_sell
+            threshold=self.config.threshold_ma200_sell,
+            sell_fraction=self.config.sell_fraction['medium']
             ) if self.config.enable_ma200 else (0.0, "MA200: N/A")
         
         portfolio_sell = sell_portfolio(
             portfolio_current=self.state.portfolio,
             warmup_invest=self.config.warmup_invest,
-            threshold=self.config.threshold_invest_years
+            threshold=self.config.threshold_invest_years,
+            sell_fraction=self.config.sell_fraction['high']
         ) if self.config.enable_portfolio else (0.0, "Limit: N/A")
 
         zscore_sell = sell_zscore(
             prices=self.config.prices.loc[:date],
-            threshold=self.config.threshold_zscore_sell
+            threshold=self.config.threshold_zscore_sell,
+            sell_fraction=self.config.sell_fraction['low']
         ) if self.config.enable_zscore else (0.0, "Z-score: N/A")
 
         rsi_sell = sell_rsi(
             prices=self.config.prices.loc[:date],
-            threshold=self.config.threshold_rsi_sell
+            threshold=self.config.threshold_rsi_sell,
+            sell_fraction=self.config.sell_fraction['low']
         ) if self.config.enable_rsi else (0.0, "RSI: N/A")
 
         model_sell = sell_model(
             prices=self.config.prices.loc[:date],
-            threshold=self.config.threshold_model_sell
+            threshold=self.config.threshold_model_sell,
+            sell_fraction=self.config.sell_fraction['low']
         ) if self.config.enable_model else (0.0, "Model: N/A")
 
         signals = sorted([model_sell, ma200_sell, portfolio_sell, zscore_sell, rsi_sell], key=lambda x: x[0])
