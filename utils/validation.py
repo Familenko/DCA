9404ca324174f7f29d4ef8bd52a640fc4043f507
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2026-08-26 17:52:39 +0300

    switch on validation

--- utils/validation.py
+++ utils/validation.py
@@ -33,20 +33,12 @@ def check_fee(fee: float):
     if not (0.0 <= fee < 1.0):
         raise ValueError("fee must be between 0 and 1")
     
-def check_minimum_profit(minimum_profit: float):
-    if minimum_profit < 1.0:
-        raise ValueError("minimum_profit must be greater than or equal to 1.0")
-    
 def check_cooldown(cooldown_days: int, cooldown_wait: int):
     if cooldown_days not in range(1, 366):
         raise ValueError("cooldown_days must be between 1 and 365")
 
     if cooldown_wait not in range(1, 366):
         raise ValueError("cooldown_wait must be between 1 and 365")
-    
-def check_threshold_invest_years(threshold_invest_years: int):
-    if threshold_invest_years < 2:
-        raise ValueError("threshold_invest_years must be at least 2")
     
 def check_manual_sell_fraction(manual_sell_fraction: float):
     if manual_sell_fraction:
@@ -55,12 +47,9 @@ def check_manual_sell_fraction(manual_sell_fraction: float):
 
 
 def validation(self, variables):
-    # check_prices(self.prices)
-    # check_buy_amount(self.buy_amount)
-    # check_freq(self.freq, variables["freq_multiplier"])
-    # check_fee(self.fee)
-    # check_minimum_profit(self.minimum_profit)
-    # check_cooldown(self.cooldown_days, self.cooldown_wait)
-    # check_threshold_invest_years(self.threshold_invest_years)
-    # check_manual_sell_fraction(self.manual_sell_fraction)
-    pass
+    check_prices(self.prices)
+    check_buy_amount(self.buy_amount)
+    check_freq(self.freq, variables["freq_multiplier"])
+    check_fee(self.fee)
+    check_cooldown(self.cooldown_days, self.cooldown_wait)
+    check_manual_sell_fraction(self.manual_sell_fraction)
